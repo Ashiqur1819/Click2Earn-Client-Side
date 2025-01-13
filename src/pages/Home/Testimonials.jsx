58d83d3146c147da8c25c68e2d61d717ecb7f095
Ashiqur1819 <ashiqurrahman1819@gmail.com>
2025-01-13 23:13:07 +0600

    Added testimonials section

--- src/pages/Home/Testimonials.jsx
+++ src/pages/Home/Testimonials.jsx
@@ -4,7 +4,6 @@ import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import { RiDoubleQuotesL } from "react-icons/ri";
-import { Rating } from "react-simple-star-rating";
 import client1 from "../../assets/client1.png"
 import client2 from "../../assets/client2.png"
 import client3 from "../../assets/client3.png"
@@ -12,11 +11,14 @@ import client3 from "../../assets/client3.png"
 const Testimonials = () => {
   return (
     <div>
-      <h2
-        className="text-2xl md:text-3xl lg:text-4xl font-bold text-black text-center"
-      >
+      <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold text-black text-center">
         What Viewers Are Saying
       </h2>
+      <p className="max-w-3xl mx-auto text-center mt-3 text-gray-800">
+        This section showcases genuine testimonials and feedback from users,
+        sharing their positive experiences to build trust and credibility with
+        potential customers.
+      </p>
       <div className="mt-12">
         <Swiper
           effect={"coverflow"}
@@ -36,7 +38,7 @@ const Testimonials = () => {
         >
           <SwiperSlide>
             <div className="max-w-3xl mx-auto flex flex-col items-center gap-3 text-center py-12">
-              <RiDoubleQuotesL className="text-3xl text-green-600"></RiDoubleQuotesL>
+              <RiDoubleQuotesL className="text-3xl text-text-primary"></RiDoubleQuotesL>
               <p>
                 I had an amazing experience browsing this website! The layout is
                 clean and modern, and everything is easy to find. The movie
@@ -45,16 +47,51 @@ const Testimonials = () => {
                 and rating. It’s a perfect site for movie lovers like me who
                 want quick access to the best films. Keep up the great work!
               </p>
-              <Rating initialValue={5}></Rating>
               <div className="flex items-center gap-3">
                 <img src={client1} className="w-16" alt="" />
                 <div>
-                  <h3>
-                    Alex Johnson
-                  </h3>
-                  <p>
-                    New York, USA
-                  </p>
+                  <h3>Alex Johnson</h3>
+                  <p>New York, USA</p>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="max-w-3xl mx-auto flex flex-col items-center gap-3 text-center py-12">
+              <RiDoubleQuotesL className="text-3xl text-text-primary"></RiDoubleQuotesL>
+              <p>
+                I had an amazing experience browsing this website! The layout is
+                clean and modern, and everything is easy to find. The movie
+                recommendations are spot on, and I love how fast the site loads.
+                I also appreciated the filter options to find movies by genre
+                and rating. It’s a perfect site for movie lovers like me who
+                want quick access to the best films. Keep up the great work!
+              </p>
+              <div className="flex items-center gap-3">
+                <img src={client2} className="w-16" alt="" />
+                <div>
+                  <h3>Alex Johnson</h3>
+                  <p>New York, USA</p>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="max-w-3xl mx-auto flex flex-col items-center gap-3 text-center py-12">
+              <RiDoubleQuotesL className="text-3xl text-text-primary"></RiDoubleQuotesL>
+              <p>
+                I had an amazing experience browsing this website! The layout is
+                clean and modern, and everything is easy to find. The movie
+                recommendations are spot on, and I love how fast the site loads.
+                I also appreciated the filter options to find movies by genre
+                and rating. It’s a perfect site for movie lovers like me who
+                want quick access to the best films. Keep up the great work!
+              </p>
+              <div className="flex items-center gap-3">
+                <img src={client3} className="w-16" alt="" />
+                <div>
+                  <h3>Alex Johnson</h3>
+                  <p>New York, USA</p>
                 </div>
               </div>
             </div>
