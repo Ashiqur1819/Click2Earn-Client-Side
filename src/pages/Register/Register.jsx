a67c6ece14d144b082cd630e667b48f9ea8b5d52
Ashiqur1819 <ashiqurrahman1819@gmail.com>
2025-01-14 11:06:59 +0600

    Done user registration functionality

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,83 +1,156 @@
-import registerImage from "../../assets/register.jpg"
+import { useState } from "react";
+import registerImage from "../../assets/register.jpg";
+import useAuth from "../../hooks/useAuth";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-    return (
-      <div className="min-h-screen">
-        <div className="grid grid-cols-1 md:grid-cols-5 gap-6 items-center max-w-6xl mx-auto mt-12 p-12">
-          <div className="col-span-2 bg-white h-full flex items-center justify-center">
-            <img src={registerImage} alt="Login image" />
-          </div>
-          <div className="card w-full bg-white p-12 h-full rounded-none col-span-3">
-            <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold text-black">
-              Create Your Account
-            </h2>
-            <form className="mt-6">
-              <div className="form-control mt-1">
-                <label className="label px-0">
-                  <span className="label-text font-medium">Name:</span>
-                </label>
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Enter Your Name"
-                  className="grow text-gray-700 text-base input border border-gray-200 rounded-none focus:border-pink-300 focus:outline-none"
-                  required
-                />
-              </div>
-              <div className="form-control mt-1">
-                <label className="label px-0">
-                  <span className="label-text font-medium">Email:</span>
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="Enter Your Email"
-                  className="grow text-gray-700 text-base input border border-gray-200 rounded-none focus:border-pink-300 focus:outline-none"
-                  required
-                />
-              </div>
-              <div className="form-control mt-1">
-                <label className="label px-0">
-                  <span className="label-text font-medium">Photo URL:</span>
-                </label>
-                <input
-                  type="url"
-                  name="photo"
-                  placeholder="Enter Your Photo URL"
-                  className="grow text-gray-700 text-base input border border-gray-200 rounded-none focus:border-pink-300 focus:outline-none"
-                  required
-                />
-              </div>
-              <div className="form-control mt-1">
-                <label className="label px-0">
-                  <span className="label-text font-medium">Password:</span>
-                </label>
-                <input
-                  type="password"
-                  placeholder="Enter Your Password"
-                  className="grow text-gray-700 text-base input border border-gray-200 rounded-none focus:border-pink-300 focus:outline-none"
-                  required
-                />
-              </div>
-              <div className="form-control mt-1">
-                <label className="label px-0">
-                  <span className="label-text font-medium">Role:</span>
-                </label>
-                <select className="select text-gray-700 text-base input border border-gray-200 rounded-none focus:border-pink-300 focus:outline-none">
-                  <option className="text-gray-700">Worker</option>
-                  <option className="text-gray-700">Buyer</option>
-                </select>
-              </div>
-              <div className="form-control mt-6">
-                <button className="bg-bg-tertiary px-4 py-2 rounded-sm text-white font-medium transition-all hover:bg-[#e6025b]">
-                  Register
-                </button>
-              </div>
-            </form>
-          </div>
+  const {setUser, createNewUser,updateUserProfile, setLoading } = useAuth();
+  const [showPassword, setShowPassword] = useState(false);
+  const [selected, setSelected] = useState();
+  const navigate = useNavigate()
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    const name = form.name.value;
+    const email = form.email.value;
+    const photo = form.photo.value;
+    const password = form.password.value;
+    const role = selected;
+    let coins = null;
+
+    if (role === "Worker") {
+      coins = 10;
+    } else {
+      coins = 50;
+    }
+
+    if (password.length < 6) {
+      return toast.error("Password must be at least 6 characters long!");
+    }
+    if (!/[A-Z]/.test(password)) {
+      return toast.error(
+        "Password must contain at least one uppercase letter!"
+      );
+    }
+
+    if (!/[a-z]/.test(password)) {
+      return toast.error(
+        "Password must contain at least one lowercase letter!"
+      );
+    }
+
+    createNewUser(email, password)
+    .then(result => {
+      setUser(result.user)
+      toast.success(`Registration successful!`)
+
+       updateUserProfile({ displayName: name, photoURL: photo })
+       .then(() => {
+        setUser((prev) => ({
+          ...prev,
+          displayName: name,
+          photoURL: photo,
+        }))
+        navigate("/")
+        setLoading(false);
+       })
+    })
+    // console.log({ name, email, photo, password, role, coins });
+  };
+
+  return (
+    <div className="min-h-screen">
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-6 items-center max-w-6xl mx-auto mt-12 p-12">
+        <div className="col-span-2 bg-white h-full flex items-center justify-center">
+          <img src={registerImage} alt="Login image" />
+        </div>
+        <div className="card w-full bg-white p-12 h-full rounded-none col-span-3">
+          <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold text-black">
+            Create Your Account
+          </h2>
+          <form onSubmit={handleRegister} className="mt-6">
+            <div className="form-control mt-1">
+              <label className="label px-0">
+                <span className="label-text font-medium">Name:</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter Your Name"
+                className="grow text-gray-700 text-base input border border-gray-200 rounded-none focus:border-pink-300 focus:outline-none"
+                required
+              />
+            </div>
+            <div className="form-control mt-1">
+              <label className="label px-0">
+                <span className="label-text font-medium">Email:</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter Your Email"
+                className="grow text-gray-700 text-base input border border-gray-200 rounded-none focus:border-pink-300 focus:outline-none"
+                required
+              />
+            </div>
+            <div className="form-control mt-1">
+              <label className="label px-0">
+                <span className="label-text font-medium">Photo URL:</span>
+              </label>
+              <input
+                type="url"
+                name="photo"
+                placeholder="Enter Your Photo URL"
+                className="grow text-gray-700 text-base input border border-gray-200 rounded-none focus:border-pink-300 focus:outline-none"
+                required
+              />
+            </div>
+            <div className="form-control relative mt-1">
+              <label className="label px-0">
+                <span className="label-text font-medium">Password:</span>
+              </label>
+              <input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                placeholder="Enter Your Password"
+                className="grow text-gray-700 text-base input border border-gray-200 rounded-none focus:border-pink-300 focus:outline-none"
+                required
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute right-4 top-[53px] text-gray-500"
+              >
+                {showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
+              </button>
+            </div>
+            <div className="form-control mt-1">
+              <label className="label px-0">
+                <span className="label-text font-medium">Role:</span>
+              </label>
+              <select
+                onChange={(e) => setSelected(e.target.value)}
+                className="select text-gray-700 text-base input border border-gray-200 rounded-none focus:border-pink-300 focus:outline-none"
+              >
+                <option className="text-gray-700">Worker</option>
+                <option className="text-gray-700">Buyer</option>
+              </select>
+            </div>
+            <div className="form-control mt-6">
+              <button className="bg-bg-tertiary px-4 py-2 rounded-sm text-white font-medium transition-all hover:bg-[#e6025b]">
+                Register
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Register;
---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,90 @@
+import { createContext, useEffect, useState } from "react";
+import auth from "../firebase/firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+
+
+export const AuthContext = createContext();
+const googleProvider = new GoogleAuthProvider();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // Register
+  const createNewUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // Login
+  const userLogin = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // Google Login
+  const loginWithGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  // Logout
+  const logOut = () => {
+    signOut(auth).then((result) => {
+      setUser(result);
+      return;
+    });
+  };
+
+  //  Forget password
+  const forgetPassword = (email) => {
+    setLoading(true);
+    return sendPasswordResetEmail(auth, email);
+  };
+
+  // Update User Profile
+  const updateUserProfile = (updatedData) => {
+    return updateProfile(auth.currentUser, updatedData);
+  };
+
+  // On State change
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+
+  const authInfo = {
+    createNewUser,
+    userLogin,
+    loginWithGoogle,
+    user,
+    setUser,
+    updateUserProfile,
+    logOut,
+    loading,
+    setLoading,
+    forgetPassword
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
