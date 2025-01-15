43bd38f20c82b32a1a83a6e130ac3907b8e2b1ab
Ashiqur1819 <ashiqurrahman1819@gmail.com>
2025-01-15 22:42:43 +0600

    Fetch all tasks in manage tasks page  and done its design

--- src/pages/dashboard/Admin/ManageTasks/ManageTasks.jsx
+++ src/pages/dashboard/Admin/ManageTasks/ManageTasks.jsx
@@ -1,6 +1,45 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxios from "../../../../hooks/useAxios";
+import { FaTrash } from "react-icons/fa";
 
 
 const ManageTasks = () => {
+
+  const axiosInstance = useAxios();
+  const { data: tasks = [] } = useQuery({
+    queryKey: ["tasks"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/tasks");
+      return res.data;
+    },
+  });
+
+  const handleDeleteTask = (task) => {
+        Swal.fire({
+          title: "Are you sure?",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, delete it!",
+        }).then(async (result) => {
+          if (result.isConfirmed) {
+            const res = await axiosInstance.delete(`/tasks/${task?._id}`);
+            if (res.data.deletedCount > 0) {
+              Swal.fire({
+                  title: "Deleted!",
+                  text: "Task has been deleted successfully.",
+                  icon: "success",
+                });
+                refetch()
+              }
+          }
+        });
+      };
+
+  
+
     return (
       <div className="mt-12 p-6 w-11/12 mx-auto bg-white rounded-sm">
         <h2 className="text-2xl md:text-3xl font-bold">Manage Tasks</h2>
@@ -10,33 +49,35 @@ const ManageTasks = () => {
             <thead>
               <tr>
                 <th></th>
-                <th>Name</th>
-                <th>Job</th>
-                <th>Favorite Color</th>
+                <th className="text-base font-medium text-blue-950">Title</th>
+                <th className="text-base font-medium text-blue-950">
+                  Buyer Email
+                </th>
+                <th className="text-base font-medium text-blue-950">
+                  Completion Date
+                </th>
+                <th className="text-base font-medium text-blue-950">Action</th>
               </tr>
             </thead>
             <tbody>
-              {/* row 1 */}
-              <tr>
-                <th>1</th>
-                <td>Cy Ganderton</td>
-                <td>Quality Control Specialist</td>
-                <td>Blue</td>
-              </tr>
-              {/* row 2 */}
-              <tr>
-                <th>2</th>
-                <td>Hart Hagerty</td>
-                <td>Desktop Support Technician</td>
-                <td>Purple</td>
-              </tr>
-              {/* row 3 */}
-              <tr>
-                <th>3</th>
-                <td>Brice Swyre</td>
-                <td>Tax Accountant</td>
-                <td>Red</td>
-              </tr>
+              {tasks.map((task, index) => (
+                <tr key={task._id}>
+                  <th>{index + 1}</th>
+                  <td className="font-medium">
+                    {task?.title.substring(0, 35)}...
+                  </td>
+                  <td className="font-medium">{task?.buyerEmail}</td>
+                  <td className="font-medium">{task?.date}</td>
+                  <td>
+                    <button
+                      onClick={() => handleDeleteTask(task)}
+                      className="text-lg bg-red-600 text-white p-2 rounded-sm"
+                    >
+                      <FaTrash></FaTrash>
+                    </button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
