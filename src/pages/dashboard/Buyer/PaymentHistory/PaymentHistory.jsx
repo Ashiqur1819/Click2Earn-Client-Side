290e44132367af7eecb3a98089771080da979a10
Ashiqur1819 <ashiqurrahman1819@gmail.com>
2025-01-17 11:38:17 +0600

    Done payment history page for the buye

--- src/pages/dashboard/Buyer/PaymentHistory/PaymentHistory.jsx
+++ src/pages/dashboard/Buyer/PaymentHistory/PaymentHistory.jsx
@@ -1,6 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../../../hooks/useAuth";
+import useAxios from "../../../../hooks/useAxios";
 
 
 const PaymentHistory = () => {
+
+  const { user } = useAuth();
+  const axiosInstance = useAxios();
+  const { data: payments = [] } = useQuery({
+    queryKey: ["payments"],
+    queryFn: async () => {
+      const res = await axiosInstance.get(`/payments/${user?.email}`);
+      return res.data;
+    },
+  });
+
+  console.log(payments)
+
     return (
       <div className="mt-12 p-6 w-11/12 mx-auto bg-white rounded-sm">
         <h2 className="text-2xl md:text-3xl font-bold">Payment History</h2>
@@ -10,33 +26,22 @@ const PaymentHistory = () => {
             <thead>
               <tr>
                 <th></th>
-                <th>Name</th>
-                <th>Job</th>
-                <th>Favorite Color</th>
+                <th className="text-base font-medium text-blue-950">Email</th>
+                <th className="text-base font-medium text-blue-950">Transaction ID</th>
+                <th className="text-base font-medium text-blue-950">Date</th>
+                <th className="text-base font-medium text-blue-950">Price</th>
               </tr>
             </thead>
             <tbody>
-              {/* row 1 */}
-              <tr>
-                <th>1</th>
-                <td>Cy Ganderton</td>
-                <td>Quality Control Specialist</td>
-                <td>Blue</td>
-              </tr>
-              {/* row 2 */}
-              <tr>
-                <th>2</th>
-                <td>Hart Hagerty</td>
-                <td>Desktop Support Technician</td>
-                <td>Purple</td>
-              </tr>
-              {/* row 3 */}
-              <tr>
-                <th>3</th>
-                <td>Brice Swyre</td>
-                <td>Tax Accountant</td>
-                <td>Red</td>
-              </tr>
+              {payments.map((payment, index) => (
+                <tr key={payment._id}>
+                  <th className="font-medium text-gray-600">{index + 1}</th>
+                  <td className="font-medium text-gray-600">{payment?.email}</td>
+                  <td className="font-medium text-gray-600">{payment?.transactionId}</td>
+                  <td className="font-medium text-gray-600">{payment?.date}</td>
+                  <td className="font-medium text-gray-600">${payment?.price}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
